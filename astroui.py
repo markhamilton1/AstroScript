f4195ef787b575d359713b9b1c2da811995052c5
Mark Hamilton <markhamilton1@users.noreply.github.com>
2017-04-27 17:32:48 -0600

    Minor refactor.

--- astroui.py
+++ astroui.py
@@ -4,124 +4,119 @@ import ui
 
 
 main_view = None
+label_font = ('Menlo', 12)
+label_color = 'gray'
+content_font = ('Menlo', 18)
+content_color = 'orange'
+
+
+def make_label(x, y, w, h, text="not empty", name=None, style="label"):
+    label = ui.Label(frame=(x, y, w, h), text=text)
+    if name is not None:
+        label.name = name
+    if style == 'label':
+        label.font = label_font
+        label.text_color = label_color
+    elif style == 'content':
+        label.font = content_font
+        label.text_color = content_color
+    return label
+
+
+def make_switch(x, y, w, h, name=None):
+    switch = ui.Switch(frame=(x, y, w, h))
+    if name is not None:
+        switch.name = name
+    return switch
+
+
+def make_textfield(x, y, w, h, name=None):
+    textfield = ui.TextField(frame=(x, y, w, h))
+    if name is not None:
+        textfield.name = name
+    return textfield
 
 
 class AstroDateView:
 
     def __init__(self):
         self.date = ad.AstroDate().alloc_now_utc()
-        self.label_font = ('<system>', 12)
-        self.content_font = ('<system>', 18)
-        self.label_color = 'gray'
         self.width = 300
 
     def make_astrodate_panel(self):
         v = ui.View(background_color='clear', name="datetime_view")
     
         x, y, w, h = 0, 0, self.width, 21
-        date_label = ui.Label(frame=(x, y, w, h), text="Current Date/Time")
-        date_label.font = self.label_font
-        date_label.text_color = self.label_color
+        date_label = make_label(x, y, w, h, text="Current Date/Time")
         v.add_subview(date_label)
     
         x, y, w, h = 0, y + h, self.width, 30
-        date_textfield = ui.TextField(frame=(x, y, w, h))
+        date_textfield = make_textfield(x, y, w, h, name="date_textfield")
         date_textfield.delegate = self
-        date_textfield.name = "date_textfield"
         date_textfield.text = self.date.get_pretty_string()
         v.add_subview(date_textfield)
     
         x, y, w, h = 0, y + h, 100, 21
-        timezone_label = ui.Label(frame=(x, y, w, h), text="Time Zone")
-        timezone_label.font = self.label_font
-        timezone_label.text_color = self.label_color
+        timezone_label = make_label(x, y, w, h, text="Time Zone")
         v.add_subview(timezone_label)
     
         x, y, w, h = x + w, y, 50, h
-        daysave_label = ui.Label(frame=(x, y, w, h), text="Daylight")
-        daysave_label.font = self.label_font
-        daysave_label.text_color = self.label_color
+        daysave_label = make_label(x, y, w, h, text="Daylight")
         v.add_subview(daysave_label)
     
         x, y, w, h = x + w, y, 150, h
-        longitude_label = ui.Label(frame=(x, y, w, h), text="Longitude")
-        longitude_label.font = self.label_font
-        longitude_label.text_color = self.label_color
+        longitude_label = make_label(x, y, w, h, text="Longitude")
         v.add_subview(longitude_label)
     
         x, y, w, h = 0, y + h, 99, 30
-        timezone_textfield = ui.TextField(frame=(x, y, w, h))
+        timezone_textfield = make_textfield(x, y, w, h, name="timezone_textfield")
         timezone_textfield.delegate = self
-        timezone_textfield.name = "timezone_textfield"
         timezone_textfield.text = "%d" % self.date.get_zone_correction()
         v.add_subview(timezone_textfield)
     
         x, y, w, h = x + w + 1, y, 50, h
-        daysave_switch = ui.Switch(frame=(x, y, w, h))
+        daysave_switch = make_switch(x, y, w, h, name="daylight_savings_switch")
         daysave_switch.action = self.switch_changed
-        daysave_switch.name = "daylight_savings_switch"
         daysave_switch.value = self.date.get_daylight_savings()
         v.add_subview(daysave_switch)
     
         x, y, w, h = x + w + 1, y, 149, h
-        longitude_textfield = ui.TextField(frame=(x, y, w, h))
+        longitude_textfield = make_textfield(x, y, w, h, name="longitude_textfield")
         longitude_textfield.delegate = self
-        longitude_textfield.name = "longitude_textfield"
         longitude_textfield.text = "%.5f" % self.date.get_longitude()
         v.add_subview(longitude_textfield)
     
         x, y, w, h = 5, y + h + 5, self.width, 21
-        label = ui.Label(frame=(x, y, w, h))
-        label.name = "lct_label"
-        label.font = self.content_font
-        label.text_color = self.label_color
+        label = make_label(x, y, w, h, name="lct_label", style="content")
         v.add_subview(label)
     
         x, y, w, h = 5, y + h + 5, self.width, 21
-        label = ui.Label(frame=(x, y, w, h))
-        label.name = "utc_label"
-        label.font = self.content_font
-        label.text_color = self.label_color
+        label = make_label(x, y, w, h, name="utc_label", style="content")
         v.add_subview(label)
 
-        x, y, w, h = 15, y + h + 5, self.width, 21
-        label = ui.Label(frame=(x, y, w, h))
-        label.name = "utc_jd_label"
-        label.font = self.content_font
-        label.text_color = self.label_color
+        x, y, w, h = 20, y + h + 5, self.width, 21
+        label = make_label(x, y, w, h, name="utc_jd_label", style="content")
         v.add_subview(label)
 
         x, y, w, h = 5, y + h + 5, self.width, 21
-        label = ui.Label(frame=(x, y, w, h))
-        label.name = "tdt_label"
-        label.font = self.content_font
-        label.text_color = self.label_color
+        label = make_label(x, y, w, h, name="tdt_label", style="content")
         v.add_subview(label)
 
-        x, y, w, h = 15, y + h + 5, self.width, 21
-        label = ui.Label(frame=(x, y, w, h))
-        label.name = "tdt_jd_label"
-        label.font = self.content_font
-        label.text_color = self.label_color
+        x, y, w, h = 20, y + h + 5, self.width, 21
+        label = make_label(x, y, w, h, name="tdt_jd_label", style="content")
         v.add_subview(label)
             
         x, y, w, h = 5, y + h + 5, self.width, 21
-        label = ui.Label(frame=(x, y, w, h))
-        label.name = "gst_label"
-        label.font = self.content_font
-        label.text_color = self.label_color
+        label = make_label(x, y, w, h, name="gst_label", style="content")
         v.add_subview(label)
 
         x, y, w, h = 5, y + h + 5, self.width, 21
-        label = ui.Label(frame=(x, y, w, h))
-        label.name = "lst_label"
-        label.font = self.content_font
-        label.text_color = self.label_color
+        label = make_label(x, y, w, h, name="lst_label", style="content")
         v.add_subview(label)
                    
         v.frame = (0, 0, self.width, y + h)
         return v
-    
+
     def set_text_in_label(self, name, text):
         dtv = main_view["datetime_view"]
         if dtv is not None:
